8601aa5a6cc0e1961a5beb7d75f658b0e7800daa
Toby Jackson <toby@tobyjackson.net>
2013-11-02 12:36:55 +0000

    Made a traffic light.
    Pin 1.0 - Red
    Pin 1.1 - Yellow
    Pin 1.1 - Green
    
    follows a (i belive) correct sequence from Red to Green (including the
    double red/yellow bit) and changes rate by swapping the trigger count
    during the interupt.

--- TheInternet/main.c
+++ TheInternet/main.c
@@ -3,45 +3,48 @@
 
 //How many cycles of the clock before we trigger the interupt
 // The VLO is around 12000/second, though environmental conditions change this
-#define TRIGGER 12000
-#define LED 0x01
+#define LONG_PAUSE 12000
+#define SHORT_PAUSE 2500
+#define R_LED 0x01
+#define Y_LED 0x02
+#define G_LED 0x04
 
 
 void main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
 
-		/* Set the entire port P1 to output to reduce power consumption. */
-		P1DIR = 0xff;
-		P1OUT = LED;
-		// the VLO must be enabled by setting bits 4 and 5 (LFXT1S) to 2 in the Basic Clock System Control Register 3 (BCSCTL3), as follows:
-		BCSCTL3 |= LFXT1S_2; //Enable the build in VLO clock
-		TACCR0 = TRIGGER;
+	/* Set the entire port P1 to output to reduce power consumption. */
+	P1DIR = 0xff;
+	P1OUT = R_LED | Y_LED | G_LED;
+	// the VLO must be enabled by setting bits 4 and 5 (LFXT1S) to 2 in the Basic Clock System Control Register 3 (BCSCTL3), as follows:
+	BCSCTL3 |= LFXT1S_2; //Enable the build in VLO clock
+	TACCR0 = LONG_PAUSE;
 
-		// We want to be notified when the timer resets, so we enable the register 0
-		// interrupt by setting bit 4 (CCIE) of the Timer_A Capture/Compare Control Register 0 (TACCTL0).
-		// This will cause a wakeup event each time the count-up timer reaches the value stored in TACCR0,
-		// as well as the execution of the Timer_A interrupt handler if it is defined.
-		TACCTL0 |= CCIE;
+	// We want to be notified when the timer resets, so we enable the register 0
+	// interrupt by setting bit 4 (CCIE) of the Timer_A Capture/Compare Control Register 0 (TACCTL0).
+	// This will cause a wakeup event each time the count-up timer reaches the value stored in TACCR0,
+	// as well as the execution of the Timer_A interrupt handler if it is defined.
+	TACCTL0 |= CCIE;
 
-		// Once the timer trigger value is set and its interrupt enabled, we must configure it for count-up
-		// mode and start the timer. These operations are both performed in the Timer_A Control Register, TACTL.
-		// To select ACLK as our clock source, we set bits 8 and 9 (TASSEL) to 01, and to enable the timer
-		//in count-up mode, we set bits 4 and 5 (MC) to 01 as well:
-		TACTL = TASSEL_1 | MC_1;
-
-
-		// Clear the timer and enable timer interrupt
-		/* Enable interrupts */
-		__enable_interrupt();
+	// Once the timer trigger value is set and its interrupt enabled, we must configure it for count-up
+	// mode and start the timer. These operations are both performed in the Timer_A Control Register, TACTL.
+	// To select ACLK as our clock source, we set bits 8 and 9 (TASSEL) to 01, and to enable the timer
+	//in count-up mode, we set bits 4 and 5 (MC) to 01 as well:
+	TACTL = TASSEL_1 | MC_1;
 
 
-		__bis_SR_register(LPM3_bits); // LPM0 with interrupts enabled
+	// Clear the timer and enable timer interrupt
+	/* Enable interrupts */
+	__enable_interrupt();
 
-		/* Unreached.  If this code were reached, the LED would stay
-		 * continuously lit. */
-		while (1)
-			P1OUT = LED;
+
+	__bis_SR_register(LPM3_bits); // LPM0 with interrupts enabled
+
+	/* Unreached.  If this code were reached, the LED would stay
+	 * continuously lit. */
+	while (1)
+		P1OUT =0x0;
 }
 
 
@@ -49,5 +52,19 @@ void main(void)
 #pragma vector=TIMERA0_VECTOR
 __interrupt void Timer_A (void)
 {
-	P1OUT ^= LED;
+	if (P1OUT == R_LED){
+		P1OUT =  R_LED | Y_LED;
+	}else if (P1OUT == (R_LED | Y_LED)){
+		P1OUT = Y_LED;
+	}else if(P1OUT == Y_LED){
+		P1OUT =  G_LED;
+	}else{
+		P1OUT = R_LED ;
+	}
+
+	if (TACCR0 == LONG_PAUSE){
+		TACCR0 = SHORT_PAUSE;
+	}else{
+		TACCR0 = LONG_PAUSE;
+	}
 }
